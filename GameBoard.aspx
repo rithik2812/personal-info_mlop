15dbca740007b7619b8bbde5f4eaaadf9402c898
rithik2812 <130275809+rithik2812@users.noreply.github.com>
2024-08-15 21:01:06 +0530

    Add files via upload

---
+++ GameBoard.aspx
@@ -0,0 +1,132 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GameBoard.aspx.cs" Inherits="chesschess.GameBoard" Async="true" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>CHESS GAME</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <style>
+        #chessBoard {
+            width: 400px;
+            height: 400px;
+            display: grid;
+            grid-template-columns: repeat(8, 1fr);
+            grid-template-rows: repeat(8, 1fr);
+            margin: 20px auto;
+            font-size: 2em;
+        }
+        .square {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: relative;
+        }
+        .white { background-color: #eee; }
+        .green { background-color: #4caf50; }
+        .piece {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            font-size: 48px;
+        }
+        .piece.white {
+            color: #fff;
+        }
+        .piece.black {
+            color: #000;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="chessBoard"></div>
+    </form>
+    <script>
+        function drawBoard(pieces) {
+            let boardHtml = '';
+            for (let i = 0; i < 64; i++) {
+                let row = Math.floor(i / 8);
+                let color = (row % 2 === 0) ? (i % 2 === 0 ? 'green' : 'white') : (i % 2 === 0 ? 'white' : 'green');
+                boardHtml += `<div class='square ${color}' id='square${i}'></div>`;
+            }
+            $('#chessBoard').html(boardHtml);
+
+            if (pieces) {
+                pieces.forEach(piece => {
+                    let squareIndex = piece.Position;
+                    let pieceColorClass = piece.IsWhite ? 'white' : 'black';
+                    let pieceHtml = `<div class='piece ${pieceColorClass}' id='piece${squareIndex}' data-piece-id='${piece.Position}'>${piece.Symbol}</div>`;
+                    $(`#square${squareIndex}`).append(pieceHtml);
+                });
+            }
+
+            $('.piece').draggable({
+                revert: "invalid",
+                start: function () {
+                    $(this).css('z-index', 1000);
+                },
+                stop: function () {
+                    $(this).css('z-index', '');
+                }
+            });
+
+            $('.square').droppable({
+                accept: '.piece',
+                drop: function (event, ui) {
+                    let pieceId = ui.draggable.data('piece-id');
+                    let targetSquare = $(this).attr('id').replace('square', '');
+
+                    movePiece(pieceId, targetSquare);
+                }
+            });
+        }
+
+        function movePiece(pieceId, targetSquare) {
+            $.ajax({
+                url: '/GameBoard.aspx/MovePiece',
+                method: 'POST',
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                data: JSON.stringify({ pieceId: pieceId, targetSquare: targetSquare }),
+                success: function (data) {
+                    if (data.d.success) {
+                        fetchBoardData();
+                    } else {
+                        alert('Invalid move.');
+                    }
+                },
+                error: function () {
+                    alert('Failed to move the piece.');
+                }
+            });
+        }
+
+        function fetchBoardData() {
+            $.ajax({
+                url: '/GameBoard.aspx/GetBoardState',
+                method: 'POST',
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                success: function (data) {
+                    drawBoard(data.d.pieces);
+                },
+                error: function () {
+                    alert('Failed to fetch board data.');
+                }
+            });
+        }
+
+        $(document).ready(function () {
+            fetchBoardData();
+        });
+    </script>
+</body>
+</html>
---
+++ GameBoard.aspx.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Services;
+
+namespace chesschess
+{
+    public partial class GameBoard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                InitializeGame();
+            }
+        }
+
+        protected void NewGameButton_Click(object sender, EventArgs e)
+        {
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            ChessGame game = new ChessGame();
+            Session["GameState"] = game;
+        }
+
+        [WebMethod]
+        public static object GetBoardState()
+        {
+            ChessGame game = (ChessGame)HttpContext.Current.Session["GameState"];
+            return new
+            {
+                pieces = game.Pieces
+            };
+        }
+
+        [WebMethod]
+        public static object MovePiece(int pieceId, int targetSquare)
+        {
+            ChessGame game = (ChessGame)HttpContext.Current.Session["GameState"];
+            ChessPiece piece = game.Pieces.Find(p => p.Position == pieceId);
+
+            if (piece != null)
+            {
+                bool isWhitePiece = piece.Symbol.StartsWith("♙") || piece.Symbol.StartsWith("♖") || piece.Symbol.StartsWith("♘") || piece.Symbol.StartsWith("♗") || piece.Symbol.StartsWith("♕") || piece.Symbol.StartsWith("♔");
+                bool isBlackPiece = !isWhitePiece;
+
+                if ((game.IsWhiteTurn && isWhitePiece) || (!game.IsWhiteTurn && isBlackPiece))
+                {
+                    if (IsMoveValid(piece, targetSquare, game))
+                    {
+                        ChessPiece targetPiece = game.Pieces.Find(p => p.Position == targetSquare);
+                        if (targetPiece != null)
+                        {
+                            game.CapturedPieces.Add(targetPiece);
+                            game.Pieces.Remove(targetPiece);
+                        }
+
+                        piece.Position = targetSquare;
+                        game.IsWhiteTurn = !game.IsWhiteTurn;
+                        HttpContext.Current.Session["GameState"] = game;
+                        return new { success = true };
+                    }
+                }
+            }
+
+            return new { success = false };
+        }
+
+        [WebMethod]
+        public static object GetCurrentTurn()
+        {
+            ChessGame game = (ChessGame)HttpContext.Current.Session["GameState"];
+            return new { isWhiteTurn = game.IsWhiteTurn };
+        }
+
+        private static bool IsMoveValid(ChessPiece piece, int targetSquare, ChessGame game)
+        {
+            int currentRow = piece.Position / 8;
+            int currentCol = piece.Position % 8;
+            int targetRow = targetSquare / 8;
+            int targetCol = targetSquare % 8;
+
+            int rowDifference = Math.Abs(targetRow - currentRow);
+            int colDifference = Math.Abs(targetCol - currentCol);
+
+            ChessPiece targetPiece = game.Pieces.Find(p => p.Position == targetSquare);
+
+            switch (piece.Name)
+            {
+                case "Pawn":
+                    if (piece.Symbol == "♙")
+                    {
+                        if (currentRow == 1 && targetRow == 3 && colDifference == 0 && targetPiece == null && IsEmptySquare(targetRow, targetCol, game))
+                            return true;
+                        if (targetRow == currentRow + 1 && colDifference == 0 && targetPiece == null)
+                            return true;
+                        if (targetRow == currentRow + 1 && colDifference == 1 && targetPiece != null && targetPiece.Symbol.StartsWith("♟"))
+                            return true; 
+                        if (targetRow == currentRow + 1 && colDifference == 1 && targetPiece == null && IsEnPassant(piece.Position, targetSquare, game))
+                            return true;
+                    }
+                    else if (piece.Symbol == "♟") 
+                    {
+                        if (currentRow == 6 && targetRow == 4 && colDifference == 0 && targetPiece == null && IsEmptySquare(targetRow, targetCol, game))
+                            return true;
+                        if (targetRow == currentRow - 1 && colDifference == 0 && targetPiece == null)
+                            return true;
+                        if (targetRow == currentRow - 1 && colDifference == 1 && targetPiece != null && targetPiece.Symbol.StartsWith("♙"))
+                            return true;
+                        if (targetRow == currentRow - 1 && colDifference == 1 && targetPiece == null && IsEnPassant(piece.Position, targetSquare, game))
+                            return true;
+                    }
+                    break;
+
+                case "Rook":
+                    if ((rowDifference == 0 || colDifference == 0) && IsPathClear(piece.Position, targetSquare, game))
+                        return true;
+                    break;
+
+                case "Knight":
+                    if ((rowDifference == 2 && colDifference == 1) || (rowDifference == 1 && colDifference == 2))
+                        return true;
+                    break;
+
+                case "Bishop":
+                    if (rowDifference == colDifference && IsPathClear(piece.Position, targetSquare, game))
+                        return true;
+                    break;
+
+                case "Queen":
+                    if ((rowDifference == colDifference || rowDifference == 0 || colDifference == 0) && IsPathClear(piece.Position, targetSquare, game))
+                        return true;
+                    break;
+
+                case "King":
+                    if (rowDifference <= 1 && colDifference <= 1)
+                        return true;
+                    if (IsCastling(piece, targetSquare, game))
+                        return true;
+                    break;
+            }
+
+            return false;
+        }
+
+        private static bool IsPathClear(int startSquare, int endSquare, ChessGame game)
+        {
+            int startRow = startSquare / 8;
+            int startCol = startSquare % 8;
+            int endRow = endSquare / 8;
+            int endCol = endSquare % 8;
+
+            int rowDirection = Math.Sign(endRow - startRow);
+            int colDirection = Math.Sign(endCol - startCol);
+
+            int currentRow = startRow + rowDirection;
+            int currentCol = startCol + colDirection;
+
+            while (currentRow != endRow || currentCol != endCol)
+            {
+                int squareIndex = currentRow * 8 + currentCol;
+                if (game.Pieces.Exists(p => p.Position == squareIndex))
+                {
+                    return false; // Path is not clear
+                }
+
+                currentRow += rowDirection;
+                currentCol += colDirection;
+            }
+
+            return true;
+        }
+
+        private static bool IsEnPassant(int pawnPosition, int targetSquare, ChessGame game)
+        {
+            int targetRow = targetSquare / 8;
+            int targetCol = targetSquare % 8;
+            int pawnRow = pawnPosition / 8;
+            int pawnCol = pawnPosition % 8;
+
+            if (targetRow == pawnRow + 1 || targetRow == pawnRow - 1)
+            {
+                if (targetCol == pawnCol + 1 || targetCol == pawnCol - 1)
+                {
+                    ChessPiece adjacentPawn = game.Pieces.Find(p => p.Position == (pawnRow * 8) + targetCol);
+                    return adjacentPawn != null && adjacentPawn.Name == "Pawn" && adjacentPawn.Symbol != game.Pieces.Find(p => p.Position == pawnPosition).Symbol;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool IsCastling(ChessPiece king, int targetSquare, ChessGame game)
+        {
+            if (king.Name != "King")
+                return false;
+
+            int currentRow = king.Position / 8;
+            int targetRow = targetSquare / 8;
+
+            if (currentRow != targetRow)
+                return false;
+
+            int currentCol = king.Position % 8;
+            int targetCol = targetSquare % 8;
+
+            if (Math.Abs(targetCol - currentCol) != 2)
+                return false;
+
+            int rookCol = targetCol > currentCol ? 7 : 0;
+            ChessPiece rook = game.Pieces.Find(p => p.Name == "Rook" && p.Position == (currentRow * 8) + rookCol);
+
+            if (rook == null || rook.Symbol.StartsWith("♟") == king.Symbol.StartsWith("♟"))
+                return false;
+
+            int colDirection = targetCol > currentCol ? 1 : -1;
+            for (int col = currentCol + colDirection; col != targetCol; col += colDirection)
+            {
+                if (game.Pieces.Exists(p => p.Position == (currentRow * 8) + col))
+                {
+                    return false; 
+                }
+            }
+
+            bool kingMoved = game.Pieces.Exists(p => p.Name == "King" && p.Position != king.Position);
+            bool rookMoved = game.Pieces.Exists(p => p.Name == "Rook" && p.Position != rook.Position);
+
+            if (kingMoved || rookMoved)
+                return false;
+
+            return true;
+        }
+
+        private static bool IsEmptySquare(int row, int col, ChessGame game)
+        {
+            return !game.Pieces.Exists(p => p.Position == (row * 8) + col);
+        }
+    }
+
+    public class ChessGame
+    {
+        public List<ChessPiece> Pieces { get; set; }
+        public List<ChessPiece> CapturedPieces { get; set; }
+        public bool IsWhiteTurn { get; set; }
+
+        public ChessGame()
+        {
+            Pieces = InitializeBoard();
+            CapturedPieces = new List<ChessPiece>(); 
+            IsWhiteTurn = true; 
+        }
+
+        private List<ChessPiece> InitializeBoard()
+        {
+            var pieces = new List<ChessPiece>();
+
+            pieces.Add(new ChessPiece("Rook", "♖", 0));   
+            pieces.Add(new ChessPiece("Knight", "♘", 1)); 
+            pieces.Add(new ChessPiece("Bishop", "♗", 2)); 
+            pieces.Add(new ChessPiece("Queen", "♕", 3)); 
+            pieces.Add(new ChessPiece("King", "♔", 4));   
+            pieces.Add(new ChessPiece("Bishop", "♗", 5)); 
+            pieces.Add(new ChessPiece("Knight", "♘", 6)); 
+            pieces.Add(new ChessPiece("Rook", "♖", 7));   
+            for (int i = 8; i < 16; i++) pieces.Add(new ChessPiece("Pawn", "♙", i));
+
+            pieces.Add(new ChessPiece("Rook", "♜", 56));  
+            pieces.Add(new ChessPiece("Knight", "♞", 57));
+            pieces.Add(new ChessPiece("Bishop", "♝", 58));
+            pieces.Add(new ChessPiece("Queen", "♛", 59)); 
+            pieces.Add(new ChessPiece("King", "♚", 60));  
+            pieces.Add(new ChessPiece("Bishop", "♝", 61));
+            pieces.Add(new ChessPiece("Knight", "♞", 62));
+            pieces.Add(new ChessPiece("Rook", "♜", 63)); 
+            for (int i = 48; i < 56; i++) pieces.Add(new ChessPiece("Pawn", "♟", i)); 
+            return pieces;
+        }
+    }
+
+    public class ChessPiece
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public int Position { get; set; }
+
+        public ChessPiece(string name, string symbol, int position)
+        {
+            Name = name;
+            Symbol = symbol;
+            Position = position;
+        }
+    }
+}
